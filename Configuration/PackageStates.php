6ab72edfb0f31fb1c31945877b524fe2180c57c9
FLOW3Distributor <mario@rimann.org>
2012-02-21 22:36:43 +0100

    Setting up the initial distribution

---
+++ Configuration/PackageStates.php
@@ -0,0 +1,60 @@
+<?php
+# This is a generated file, do not edit manually
+# Use the commands flow3:package:activate and flow3:package:deactivate for changes
+return array (
+  'packages' => 
+  array (
+    'Doctrine.Common' => 
+    array (
+      'state' => 'active',
+      'packagePath' => '/home/jk/Projekte/FLOW3Distributor/Gucken.Org/Packages/Framework/Doctrine.Common/',
+    ),
+    'TYPO3.Party' => 
+    array (
+      'state' => 'active',
+      'packagePath' => '/home/jk/Projekte/FLOW3Distributor/Gucken.Org/Packages/Framework/TYPO3.Party/',
+    ),
+    'Doctrine.ORM' => 
+    array (
+      'state' => 'active',
+      'packagePath' => '/home/jk/Projekte/FLOW3Distributor/Gucken.Org/Packages/Framework/Doctrine.ORM/',
+    ),
+    'Symfony.Component.Yaml' => 
+    array (
+      'state' => 'active',
+      'packagePath' => '/home/jk/Projekte/FLOW3Distributor/Gucken.Org/Packages/Framework/Symfony.Component.Yaml/',
+    ),
+    'TYPO3.Fluid' => 
+    array (
+      'state' => 'active',
+      'packagePath' => '/home/jk/Projekte/FLOW3Distributor/Gucken.Org/Packages/Framework/TYPO3.Fluid/',
+    ),
+    'Doctrine.DBAL' => 
+    array (
+      'state' => 'active',
+      'packagePath' => '/home/jk/Projekte/FLOW3Distributor/Gucken.Org/Packages/Framework/Doctrine.DBAL/',
+    ),
+    'TYPO3.FLOW3' => 
+    array (
+      'state' => 'active',
+      'packagePath' => '/home/jk/Projekte/FLOW3Distributor/Gucken.Org/Packages/Framework/TYPO3.FLOW3/',
+    ),
+    'TYPO3.Kickstart' => 
+    array (
+      'state' => 'active',
+      'packagePath' => '/home/jk/Projekte/FLOW3Distributor/Gucken.Org/Packages/Framework/TYPO3.Kickstart/',
+    ),
+    'Admin' => 
+    array (
+      'state' => 'active',
+      'packagePath' => '/home/jk/Projekte/FLOW3Distributor/Gucken.Org/Packages/Application/Admin/',
+    ),
+    'Org.Gucken.Events' => 
+    array (
+      'state' => 'active',
+      'packagePath' => '/home/jk/Projekte/FLOW3Distributor/Gucken.Org/Packages/Application/Org.Gucken.Events/',
+    ),
+  ),
+  'version' => 1,
+)
+ ?>
